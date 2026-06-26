719e5eaea667c2e8ea6a0c92fe3ec684c6a79654
Olfatalatas <olfatharits@gmail.com>
2026-06-26 21:35:07 +0700

    feat: implement magazine homepage and search result views with dynamic content rendering

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -154,18 +154,6 @@
 			</div>
 		</section>
 		@endif
-		<section id="Advertisement" class="max-w-[1130px] mx-auto flex justify-center mt-[70px]">
-			<div class="flex flex-col gap-3 shrink-0 w-fit">
-				<a href="{{$bannerads->link}}">
-					<div class="w-[900px] h-[120px] flex shrink-0 border border-[#EEF0F7] rounded-2xl overflow-hidden">
-						<img src="{{Storage::url($bannerads->thumbnail)}}" class="object-cover w-full h-full" alt="ads" />
-					</div>
-				</a>
-				<p class="font-medium text-sm leading-[21px] text-[#A3A6AE] flex gap-1">
-					Our Advertisement <a href="#" class="w-[18px] h-[18px]"><img src="assets/images/icons/message-question.svg" alt="icon" /></a>
-				</p>
-			</div>
-		</section>
 		@if($business_featured_articles || $business_articles->count() > 0)
 		<section id="Latest-business" class="max-w-[1130px] mx-auto flex flex-col gap-[30px] mt-[70px]">
 			<div class="flex justify-between items-center">
@@ -212,18 +200,6 @@
 			</div>
 		</section>
 		@endif
-		<section id="Advertisement" class="max-w-[1130px] mx-auto flex justify-center mt-[70px]">
-			<div class="flex flex-col gap-3 shrink-0 w-fit">
-				<a href="{{$bannerads->link}}">
-					<div class="w-[900px] h-[120px] flex shrink-0 border border-[#EEF0F7] rounded-2xl overflow-hidden">
-						<img src="{{Storage::url($bannerads->thumbnail)}}" class="object-cover w-full h-full" alt="ads" />
-					</div>
-				</a>
-				<p class="font-medium text-sm leading-[21px] text-[#A3A6AE] flex gap-1">
-					Our Advertisement <a href="#" class="w-[18px] h-[18px]"><img src="assets/images/icons/message-question.svg" alt="icon" /></a>
-				</p>
-			</div>
-		</section>
 		@if($automotive_featured_articles || $automotive_articles->count() > 0)
 		<section id="Latest-automotive" class="max-w-[1130px] mx-auto flex flex-col gap-[30px] mt-[70px]">
 			<div class="flex justify-between items-center">
